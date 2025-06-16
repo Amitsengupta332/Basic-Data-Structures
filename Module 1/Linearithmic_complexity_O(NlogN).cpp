87c60bc4abe57de3f1ce05ef4c81cb0ea1a3e6c6
amitsengupta <amitsengupta332@gmail.com>
2025-06-16 18:47:03 +0600

    Linearithmic complexity O(NlogN)

--- Module 1/Linearithmic_complexity_O(NlogN).cpp
+++ Module 1/Linearithmic_complexity_O(NlogN).cpp
@@ -3,15 +3,14 @@ using namespace std;
 
 int main()
 {
-
     int n;
     cin >> n;
 
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= n; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = 1; j < n; j *= 2)
         {
-            cout << "Hello" << endl; // complexity O(n^2)
+            cout << "Hello" << endl; // complexity O(nlogn)
         }
     }
 
