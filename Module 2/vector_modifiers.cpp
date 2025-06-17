be868bd6569baee501381821c7cdcbaa3e09cff4
amitsengupta <amitsengupta332@gmail.com>
2025-06-18 01:11:39 +0600

    vector modifier i

--- Module 2/vector_modifiers.cpp
+++ Module 2/vector_modifiers.cpp
@@ -3,9 +3,21 @@ using namespace std;
 
 int main()
 {
-    vector<int> v = {1, 2, 3, 4, 5};
-    vector<int> v2;
-    v2 = v;
+    vector<int> v = {1, 2, 3, 4, 5, 6};
+    vector<int> v2 = {100, 200, 300};
+    // v.insert(v.begin() + 2, v2.begin(), v2.end());
+    // v.insert(v.begin() + 2, 100);
+
+    v.erase(v.begin() + 1, v.begin() + 5);
+
+    // v.pop_back();
+    // v.pop_back();
+    for (int x : v)
+    {
+        cout << x << " ";
+    }
+    // vector<int> v2;
+    // v2 = v;
     //    for (int i = 0; i < v2.size(); i++)
     //    {
     //     cout << v2[i] << " ";
@@ -13,10 +25,10 @@ int main()
     //    }
 
     //  Range based for loop
-    for (int x : v2)
-    {
-        cout << x << " ";
-    }
+    // for (int x : v2)
+    // {
+    //     cout << x << " ";
+    // }
 
     return 0;
 }
