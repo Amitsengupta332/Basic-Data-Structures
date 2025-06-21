ccfddf48e42eaccf7ef813cdefbe14a50325636f
amitsengupta <amitsengupta332@gmail.com>
2025-06-22 01:34:15 +0600

    solve tle

--- Module 3/Y_Range_sum_query.cpp
+++ Module 3/Y_Range_sum_query.cpp
@@ -12,17 +12,36 @@ int main()
         cin >> v[i];
     }
 
+    vector<int> pre(n + 1);
+    pre[1] = v[1];
+
+    for (int i = 2; i <= n; i++)
+    {
+        pre[i] = pre[i - 1] + v[i];
+        /* code */
+    }
+
     while (q--)
     {
         int l, r;
         cin >> l >> r;
-        int sum = 0;
-        for (int i = l; i <= r; i++)
+
+        int sum;
+        if (l == 1)
         {
-            sum += v[i];
+            sum = pre[r];
+        }
+        else
+        {
+            sum = pre[r] - pre[l - 1];
         }
         cout << sum << endl;
-
+        // int sum = 0;
+        // for (int i = l; i <= r; i++)
+        // {
+        //     sum += v[i];
+        // }
+        // cout << sum << endl;
     }
 
     return 0;
