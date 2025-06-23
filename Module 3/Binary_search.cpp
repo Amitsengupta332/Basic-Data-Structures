c0475dc7010ee0ea426aa7fa21824e304489b596
amitsengupta <amitsengupta332@gmail.com>
2025-06-23 19:52:14 +0600

    binary search

--- Module 3/Binary_search.cpp
+++ Module 3/Binary_search.cpp
@@ -10,5 +10,47 @@ int main()
     {
         cin >> arr[i];
     }
+    int val;
+    cin >> val;
+    int flag = 0;
+
+    // Using binary search
+    int l = 0, r = n - 1;
+
+    while ( l <= r)
+    {
+        int mid = (l + r) / 2;
+        if (arr[mid] == val)
+        {
+            flag = 1;
+            break;
+        }
+        else if (arr[mid] > val)
+        {
+            r = mid - 1;
+        }
+        else
+        {
+            l = mid + 1;
+        }
+    }
+    // for (int i = 0; i < n; i++)
+    // {
+
+    //     if (arr[i] == val)
+    //     {
+    //         flag = 1;
+    //     }
+    //     /* code */
+    // }
+
+    if (flag == 1)
+    {
+        cout << "found" << endl;
+    }
+    else
+    {
+        cout << "not found" << endl;
+    }
     return 0;
 }
