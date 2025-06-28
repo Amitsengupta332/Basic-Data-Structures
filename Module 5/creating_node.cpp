95769eabe22aa6a8e9e8b730da127753bb7c7dea
amitsengupta <amitsengupta332@gmail.com>
2025-06-29 00:06:24 +0600

    constructor node

--- Module 5/creating_node.cpp
+++ Module 5/creating_node.cpp
@@ -6,19 +6,26 @@ class Node
 public:
     int val;
     Node* next;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
 };
 
 int main()
 {
-    Node a,b,c;
+    // Node a,b,c;
+    Node  a(10), b(20), c(30);
 
-    a.val = 10;
-    b.val = 20;
-    c.val = 30;
+    // a.val = 10;
+    // b.val = 20;
+    // c.val = 30;
 
     a.next = &b;
     b.next = &c;
-    c.next = NULL;
+    // c.next = NULL;
 
     // cout << a.val << " " << b.val << " " << c.val << endl;
     cout << a.val << endl;
