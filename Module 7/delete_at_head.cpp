beca83e6db58eb4ada3e2a3c39b1c17aeb4ebd13
amitsengupta <amitsengupta332@gmail.com>
2025-07-02 01:43:49 +0600

    delete at head

---
+++ Module 7/delete_at_head.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert_a_tail(Node *&head, Node *&tail, int val) // Notice the & for reference
+{
+
+    Node *newNode = new Node(val);
+
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode; // If the list was empty, head and tail point to the new node
+        return;
+    }
+
+    tail->next = newNode;
+    tail = newNode; // Update the tail to point to the new node
+}
+
+void Print_Linked_List(Node *head)
+{
+    Node *temp = head;
+
+    while (temp != NULL)
+    {
+        cout << temp->val << endl;
+        temp = temp->next;
+    }
+}
+
+void delete_at_head(Node* &head)
+{
+    if (head == NULL)
+    {
+        return; // If the list is empty, do nothing
+    }
+    Node *deleteNode = head;
+    head = head->next;
+    delete deleteNode;
+}
+
+int main()
+{
+
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    int val;
+    while (true)
+    {
+        cin >> val;
+        if (val == -1)
+        {
+            break;
+        }
+        insert_a_tail(head, tail, val);
+    }
+
+    delete_at_head(head);  
+    delete_at_head(head);  
+    Print_Linked_List(head);
+
+    return 0;
+}
