510afbd7b5bd26f49229bd653d6f5c00f362339d
amitsengupta <amitsengupta332@gmail.com>
2025-07-04 00:34:57 +0600

    a2 fix

---
+++ Module 8 -Assignment/a2_search.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert_a_tail(Node *&head, Node *&tail, int val) // Notice the & for reference
+{
+
+    Node *newNode = new Node(val);
+
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode; // If the list was empty, head and tail point to the new node
+        return;
+    }
+
+    tail->next = newNode;
+    tail = newNode; // Update the tail to point to the new node
+}
+
+int find_Index_of_x(Node *head, int x)
+{
+    Node *temp = head;
+    int index = 0;
+    while (temp != NULL)
+    {
+        if (temp->val == x)
+        {
+            return index;
+        }
+        temp = temp->next;
+        index++;
+    }
+    return -1;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+
+        Node *head = NULL;
+        Node *tail = NULL;
+        int val;
+
+        while (true)
+        {
+            cin >> val;
+            if (val == -1)
+            {
+                break;
+            }
+            insert_a_tail(head, tail, val);
+        }
+
+        int x;
+        cin >> x;
+
+        cout << find_Index_of_x(head, x) << endl;
+        Node *temp = head;
+        while (temp != NULL)
+        {
+            Node *nextNode = temp->next;
+            delete temp;
+            temp = nextNode;
+        }
+    }
+
+    return 0;
+}
