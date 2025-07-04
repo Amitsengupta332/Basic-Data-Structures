ca9eaae9800c0cd705ee679410a456f99e94559a
amitsengupta <amitsengupta332@gmail.com>
2025-07-05 01:15:46 +0600

    done queries

--- Module 8 -Assignment/a4_queries.cpp
+++ Module 8 -Assignment/a4_queries.cpp
@@ -14,12 +14,18 @@ public:
     }
 };
 
-void insert_a_head(Node *&head, int val)  
+void insert_a_head(Node *&head, Node *&tail, int val)
 {
     Node *newNode = new Node(val);
     newNode->next = head;
     head = newNode;
+    if (tail == NULL) {
+        tail = newNode;
+    }
 }
+
+
+ 
 void insert_a_tail(Node *&head, Node *&tail, int val)
 {
 
@@ -45,24 +51,84 @@ void Print_Linked_List(Node *head)
         cout << temp->val << " ";
         temp = temp->next;
     }
+    cout << endl;
+}
+
+void delete_at_index(Node *&head, Node *&tail, int idx)
+{
+
+    if (head == NULL)
+    {
+        return;
+    }
+
+    if (idx == 0)
+    {
+        Node *delNode = head;
+        head = head->next;
+        delete delNode;
+        if (head == NULL)
+            tail = NULL;
+        return;
+    }
+
+    Node *temp = head;
+    for (int i = 0; temp != NULL && i < idx - 1; i++)
+    {
+        temp = temp->next;
+    }
+
+    if (temp == NULL || temp->next == NULL)
+        return;
+
+    Node *delNode = temp->next;
+    temp->next = delNode->next;
+
+    if (delNode == tail)
+    {
+        tail = temp;
+    }
+
+    delete delNode;
+}
+
+void deleteList(Node *head)
+{
+    while (head)
+    {
+        Node *next = head->next;
+        delete head;
+        head = next;
+    }
 }
 int main()
 {
     Node *head = NULL;
     Node *tail = NULL;
-    int val;
+    int q;
+    cin >> q;
 
-    while (true)
+    while (q--)
     {
-        cin >> val;
-        if (val == -1)
-        {
-            break;
-        }
-        insert_a_tail(head, tail, val);
-    }
+        int x, v;
+        cin >> x >> v;
 
-    Print_Linked_List(head);
+        if (x == 0)
+        {
+            insert_a_head(head, tail, v);   
+    
+        }
+        else if (x == 1)
+        {
+            insert_a_tail(head, tail, v);
+        }
+        else if (x == 2)
+        {
+            delete_at_index(head, tail, v);
+        }
+
+        Print_Linked_List(head);
+    }
 
     return 0;
 }
