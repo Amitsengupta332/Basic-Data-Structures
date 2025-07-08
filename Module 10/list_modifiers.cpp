0b0f9adcbc07148d9240d4fc4de7a5c2b172c5a4
amitsengupta <amitsengupta332@gmail.com>
2025-07-08 21:54:53 +0600

    Assign another list.

--- Module 10/list_modifiers.cpp
+++ Module 10/list_modifiers.cpp
@@ -4,5 +4,12 @@ using namespace std;
 int main()
 {
   
+    list<int> l = {10,20 ,30};
+    list<int> l2;
+    l2 = l;
+    for(int val:l2)
+    {
+        cout << val << endl;
+    }
     return 0;
 }
