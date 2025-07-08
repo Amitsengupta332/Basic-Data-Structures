e5e85904ceb510f8611444bfd9cb8c25c9f3eaf7
amitsengupta <amitsengupta332@gmail.com>
2025-07-09 00:30:37 +0600

    push back pop back

--- Module 10/list_modifiers.cpp
+++ Module 10/list_modifiers.cpp
@@ -4,12 +4,23 @@ using namespace std;
 int main()
 {
   
-    list<int> l = {10,20 ,30};
+    list<int> l = {10,20 ,30, 40};
     list<int> l2;
-    l2 = l;
-    for(int val:l2)
+    // l2 = l;
+    // l2.assign(l.begin(), l.end());
+
+    // l.push_back(50);
+    // l.push_front(5);
+
+    // l.pop_back();
+    // l.pop_back();
+    for(int val:l)
     {
         cout << val << endl;
     }
+    // for(int val:l2)
+    // {
+    //     cout << val << endl;
+    // }
     return 0;
 }
