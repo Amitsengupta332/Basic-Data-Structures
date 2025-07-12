906cba69b7a6ec031a39ac22174c6bafb77a5e29
amitsengupta <amitsengupta332@gmail.com>
2025-07-12 20:12:39 +0600

    insert a tai

---
+++ Session/week 3/insert_at_tail_corner_case.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *prev;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    tail->next = newNode;
+    newNode->prev = tail;
+    tail = newNode;
+}
+
+int main()
+{
+
+    return 0;
+}
