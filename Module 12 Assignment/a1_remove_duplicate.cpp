122f10089cc3ab1bf308edf4624d8fd73f6a2e8b
amitsengupta <amitsengupta332@gmail.com>
2025-07-17 00:27:14 +0600

    remove duplicate

--- Module 12 Assignment/a1_remove_duplicate.cpp
+++ Module 12 Assignment/a1_remove_duplicate.cpp
@@ -3,6 +3,23 @@ using namespace std;
 
 int main()
 {
-  
+    list<int> l;
+    int val;
+    cin >> val;
+
+    while (val != -1)
+    {
+        l.push_back(val);
+        cin >> val;
+    }
+
+    l.sort();
+    l.unique();
+
+    for (int v : l)
+    {
+        cout << v << " ";
+    }
+
     return 0;
 }
