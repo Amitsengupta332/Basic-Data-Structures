9daf0ebf4902b390d78588b7f731a147f6bfc529
amitsengupta <amitsengupta332@gmail.com>
2025-07-26 00:13:30 +0600

    done a4 special queriees

--- Module 16-Assignment/a4_special_queries.cpp
+++ Module 16-Assignment/a4_special_queries.cpp
@@ -3,7 +3,36 @@ using namespace std;
 
 int main()
 {
-    list<int> l;
-  
+    int q;
+    cin >> q;
+    queue<string> l;
+
+    while (q--)
+    {
+        int x;
+        cin >> x;
+
+        if (x == 0)
+        {
+            string s;
+            cin >> s;
+            l.push(s);
+        }
+        else if (x == 1)
+        {
+
+            if (!l.empty())
+            {
+                cout << l.front() << endl;
+                l.pop();
+            }
+            else
+            {
+                cout << "Invalid" << endl;
+            }
+       
+        }
+    }
+
     return 0;
 }
