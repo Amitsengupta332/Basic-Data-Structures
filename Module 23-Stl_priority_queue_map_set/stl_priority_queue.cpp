31345277e4b71b1e3aa68512623314b5b26c16d1
amitsengupta <amitsengupta332@gmail.com>
2025-08-06 00:46:44 +0600

    done priority queue

--- Module 23-Stl_priority_queue_map_set/stl_priority_queue.cpp
+++ Module 23-Stl_priority_queue_map_set/stl_priority_queue.cpp
@@ -3,12 +3,17 @@ using namespace std;
 
 int main()
 {
-    priority_queue<int> pq;
+    priority_queue<int, vector<int>, greater<int>> pq;
     pq.push(10);
     pq.push(5);
     pq.push(30);
 
     cout << pq.top() << endl;
-
+    pq.push(2);
+    // pq.push(100);
+    cout << pq.top() << endl;
+    pq.pop();
+    pq.pop();
+    cout << pq.top() << endl;
     return 0;
 }
