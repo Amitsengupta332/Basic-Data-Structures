14bcf8c1c10e0d3e1b0677b9655be941ea4684f5
amitsengupta <amitsengupta332@gmail.com>
2025-08-06 20:24:50 +0600

    done map

--- Module 23-Stl_priority_queue_map_set/map.cpp
+++ Module 23-Stl_priority_queue_map_set/map.cpp
@@ -3,6 +3,23 @@ using namespace std;
 
 int main()
 {
-  
+    map<string, int> mp;
+    // int fre[100];
+    // fre[5] = 20;
+    mp["sakib"] = 2;
+    mp["rakib"] = 3;
+    mp["tamim"] = 1;
+
+    // cout << mp["sakib"] << endl;
+
+    // for(auto it = mp.begin(); it != mp.end(); it++)
+    // {
+    //     cout << it->first << " " << it->second << endl;
+    // }
+
+    if(mp.count("sakib") > 0)
+        cout << "Ache" << endl;
+    else
+        cout << "Nai" << endl;
     return 0;
 }
