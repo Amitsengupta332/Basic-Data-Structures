2d2d1ae4fa3beca20441f79e33fbef4ded29b261
amitsengupta <amitsengupta332@gmail.com>
2025-08-08 01:21:00 +0600

    done a3

--- Module 24- Assignment/a3_count.cpp
+++ Module 24- Assignment/a3_count.cpp
@@ -3,6 +3,35 @@ using namespace std;
 
 int main()
 {
-  
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t--)
+    {
+        string s;
+        getline(cin, s);
+
+        stringstream ss(s);
+        unordered_map<string, int> freq;
+        string word;
+
+        string result = "";
+        int maxFreq = 0;
+
+        while (ss >> word)
+        {
+            freq[word]++;
+
+            if (freq[word] > maxFreq)
+            {
+                maxFreq = freq[word];
+                result = word;
+            }
+        }
+
+        cout << result << " " << maxFreq << endl;
+    }
+
     return 0;
 }
